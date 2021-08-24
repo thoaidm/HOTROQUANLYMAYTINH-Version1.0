1536f0d325b2a0c6c182a2479160e5a13441202b
thoaidm <minhthoai3@gmail.com>
2021-08-24 10:15:49 +0700

    Source Develop

---
+++ monhanhcacungdung/Form1.Designer.vb
@@ -0,0 +1,261 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.btnmothumucFonts = New System.Windows.Forms.Button()
+        Me.btnmothumucHost = New System.Windows.Forms.Button()
+        Me.btnmoregistry = New System.Windows.Forms.Button()
+        Me.btnmoGrouppolicy = New System.Windows.Forms.Button()
+        Me.btnmoServices = New System.Windows.Forms.Button()
+        Me.btnopenMsconfig = New System.Windows.Forms.Button()
+        Me.btnmoCmd = New System.Windows.Forms.Button()
+        Me.btnmoNetplwiz = New System.Windows.Forms.Button()
+        Me.btntatwindowsupdate = New System.Windows.Forms.Button()
+        Me.btnmoDestopIconSettings = New System.Windows.Forms.Button()
+        Me.btnmoAdvancedSharingSettings = New System.Windows.Forms.Button()
+        Me.btnmoAdapterOptions = New System.Windows.Forms.Button()
+        Me.btntaiTeamViewer = New System.Windows.Forms.Button()
+        Me.btnmoProgramsAndFeatures = New System.Windows.Forms.Button()
+        Me.btnmoDateAndTime = New System.Windows.Forms.Button()
+        Me.btnmoBanphimAo = New System.Windows.Forms.Button()
+        Me.btnmoMaytinhMini = New System.Windows.Forms.Button()
+        Me.btnmoPerformance = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnmothumucFonts
+        '
+        Me.btnmothumucFonts.Location = New System.Drawing.Point(12, 12)
+        Me.btnmothumucFonts.Name = "btnmothumucFonts"
+        Me.btnmothumucFonts.Size = New System.Drawing.Size(137, 29)
+        Me.btnmothumucFonts.TabIndex = 0
+        Me.btnmothumucFonts.Text = "Thư mục Fonts"
+        Me.btnmothumucFonts.UseVisualStyleBackColor = True
+        '
+        'btnmothumucHost
+        '
+        Me.btnmothumucHost.Location = New System.Drawing.Point(12, 47)
+        Me.btnmothumucHost.Name = "btnmothumucHost"
+        Me.btnmothumucHost.Size = New System.Drawing.Size(137, 29)
+        Me.btnmothumucHost.TabIndex = 0
+        Me.btnmothumucHost.Text = "Thư mục chứa file Hosts"
+        Me.btnmothumucHost.UseVisualStyleBackColor = True
+        '
+        'btnmoregistry
+        '
+        Me.btnmoregistry.Location = New System.Drawing.Point(12, 82)
+        Me.btnmoregistry.Name = "btnmoregistry"
+        Me.btnmoregistry.Size = New System.Drawing.Size(137, 29)
+        Me.btnmoregistry.TabIndex = 0
+        Me.btnmoregistry.Text = "Mở Registry"
+        Me.btnmoregistry.UseVisualStyleBackColor = True
+        '
+        'btnmoGrouppolicy
+        '
+        Me.btnmoGrouppolicy.Location = New System.Drawing.Point(12, 117)
+        Me.btnmoGrouppolicy.Name = "btnmoGrouppolicy"
+        Me.btnmoGrouppolicy.Size = New System.Drawing.Size(137, 29)
+        Me.btnmoGrouppolicy.TabIndex = 0
+        Me.btnmoGrouppolicy.Text = "Mở Group Policy"
+        Me.btnmoGrouppolicy.UseVisualStyleBackColor = True
+        '
+        'btnmoServices
+        '
+        Me.btnmoServices.Location = New System.Drawing.Point(12, 152)
+        Me.btnmoServices.Name = "btnmoServices"
+        Me.btnmoServices.Size = New System.Drawing.Size(137, 29)
+        Me.btnmoServices.TabIndex = 0
+        Me.btnmoServices.Text = "Mở Services"
+        Me.btnmoServices.UseVisualStyleBackColor = True
+        '
+        'btnopenMsconfig
+        '
+        Me.btnopenMsconfig.Location = New System.Drawing.Point(12, 187)
+        Me.btnopenMsconfig.Name = "btnopenMsconfig"
+        Me.btnopenMsconfig.Size = New System.Drawing.Size(137, 29)
+        Me.btnopenMsconfig.TabIndex = 0
+        Me.btnopenMsconfig.Text = "Mở Msconfig"
+        Me.btnopenMsconfig.UseVisualStyleBackColor = True
+        '
+        'btnmoCmd
+        '
+        Me.btnmoCmd.Location = New System.Drawing.Point(171, 12)
+        Me.btnmoCmd.Name = "btnmoCmd"
+        Me.btnmoCmd.Size = New System.Drawing.Size(218, 29)
+        Me.btnmoCmd.TabIndex = 0
+        Me.btnmoCmd.Text = "Mở CMD"
+        Me.btnmoCmd.UseVisualStyleBackColor = True
+        '
+        'btnmoNetplwiz
+        '
+        Me.btnmoNetplwiz.Location = New System.Drawing.Point(171, 47)
+        Me.btnmoNetplwiz.Name = "btnmoNetplwiz"
+        Me.btnmoNetplwiz.Size = New System.Drawing.Size(218, 29)
+        Me.btnmoNetplwiz.TabIndex = 0
+        Me.btnmoNetplwiz.Text = "Mở Netplwiz (chọn tài khoản khởi động)"
+        Me.btnmoNetplwiz.UseVisualStyleBackColor = True
+        '
+        'btntatwindowsupdate
+        '
+        Me.btntatwindowsupdate.Location = New System.Drawing.Point(171, 82)
+        Me.btntatwindowsupdate.Name = "btntatwindowsupdate"
+        Me.btntatwindowsupdate.Size = New System.Drawing.Size(218, 29)
+        Me.btntatwindowsupdate.TabIndex = 0
+        Me.btntatwindowsupdate.Text = "Disable Windows Update"
+        Me.btntatwindowsupdate.UseVisualStyleBackColor = True
+        '
+        'btnmoDestopIconSettings
+        '
+        Me.btnmoDestopIconSettings.Location = New System.Drawing.Point(171, 117)
+        Me.btnmoDestopIconSettings.Name = "btnmoDestopIconSettings"
+        Me.btnmoDestopIconSettings.Size = New System.Drawing.Size(218, 29)
+        Me.btnmoDestopIconSettings.TabIndex = 0
+        Me.btnmoDestopIconSettings.Text = "Mở Desktop Icon Settings"
+        Me.btnmoDestopIconSettings.UseVisualStyleBackColor = True
+        '
+        'btnmoAdvancedSharingSettings
+        '
+        Me.btnmoAdvancedSharingSettings.Location = New System.Drawing.Point(171, 187)
+        Me.btnmoAdvancedSharingSettings.Name = "btnmoAdvancedSharingSettings"
+        Me.btnmoAdvancedSharingSettings.Size = New System.Drawing.Size(218, 29)
+        Me.btnmoAdvancedSharingSettings.TabIndex = 0
+        Me.btnmoAdvancedSharingSettings.Text = "Mở Advanced Sharing Settings"
+        Me.btnmoAdvancedSharingSettings.UseVisualStyleBackColor = True
+        '
+        'btnmoAdapterOptions
+        '
+        Me.btnmoAdapterOptions.Location = New System.Drawing.Point(171, 152)
+        Me.btnmoAdapterOptions.Name = "btnmoAdapterOptions"
+        Me.btnmoAdapterOptions.Size = New System.Drawing.Size(218, 29)
+        Me.btnmoAdapterOptions.TabIndex = 0
+        Me.btnmoAdapterOptions.Text = "Mở Adapter Options"
+        Me.btnmoAdapterOptions.UseVisualStyleBackColor = True
+        '
+        'btntaiTeamViewer
+        '
+        Me.btntaiTeamViewer.Location = New System.Drawing.Point(410, 12)
+        Me.btntaiTeamViewer.Name = "btntaiTeamViewer"
+        Me.btntaiTeamViewer.Size = New System.Drawing.Size(218, 29)
+        Me.btntaiTeamViewer.TabIndex = 0
+        Me.btntaiTeamViewer.Text = "Tải về TeamViewer QuickSupport"
+        Me.btntaiTeamViewer.UseVisualStyleBackColor = True
+        '
+        'btnmoProgramsAndFeatures
+        '
+        Me.btnmoProgramsAndFeatures.Location = New System.Drawing.Point(410, 82)
+        Me.btnmoProgramsAndFeatures.Name = "btnmoProgramsAndFeatures"
+        Me.btnmoProgramsAndFeatures.Size = New System.Drawing.Size(218, 29)
+        Me.btnmoProgramsAndFeatures.TabIndex = 0
+        Me.btnmoProgramsAndFeatures.Text = "Gỡ cài đặt phần mềm"
+        Me.btnmoProgramsAndFeatures.UseVisualStyleBackColor = True
+        '
+        'btnmoDateAndTime
+        '
+        Me.btnmoDateAndTime.Location = New System.Drawing.Point(410, 47)
+        Me.btnmoDateAndTime.Name = "btnmoDateAndTime"
+        Me.btnmoDateAndTime.Size = New System.Drawing.Size(218, 29)
+        Me.btnmoDateAndTime.TabIndex = 0
+        Me.btnmoDateAndTime.Text = "Mở Date And Time"
+        Me.btnmoDateAndTime.UseVisualStyleBackColor = True
+        '
+        'btnmoBanphimAo
+        '
+        Me.btnmoBanphimAo.Location = New System.Drawing.Point(410, 117)
+        Me.btnmoBanphimAo.Name = "btnmoBanphimAo"
+        Me.btnmoBanphimAo.Size = New System.Drawing.Size(218, 29)
+        Me.btnmoBanphimAo.TabIndex = 0
+        Me.btnmoBanphimAo.Text = "Mở Bàn phím ảo"
+        Me.btnmoBanphimAo.UseVisualStyleBackColor = True
+        '
+        'btnmoMaytinhMini
+        '
+        Me.btnmoMaytinhMini.Location = New System.Drawing.Point(410, 152)
+        Me.btnmoMaytinhMini.Name = "btnmoMaytinhMini"
+        Me.btnmoMaytinhMini.Size = New System.Drawing.Size(218, 29)
+        Me.btnmoMaytinhMini.TabIndex = 0
+        Me.btnmoMaytinhMini.Text = "Mở Calculator"
+        Me.btnmoMaytinhMini.UseVisualStyleBackColor = True
+        '
+        'btnmoPerformance
+        '
+        Me.btnmoPerformance.Location = New System.Drawing.Point(410, 187)
+        Me.btnmoPerformance.Name = "btnmoPerformance"
+        Me.btnmoPerformance.Size = New System.Drawing.Size(218, 29)
+        Me.btnmoPerformance.TabIndex = 0
+        Me.btnmoPerformance.Text = "Mở Performance Options"
+        Me.btnmoPerformance.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.GradientActiveCaption
+        Me.ClientSize = New System.Drawing.Size(648, 234)
+        Me.Controls.Add(Me.btnmoDateAndTime)
+        Me.Controls.Add(Me.btnmoPerformance)
+        Me.Controls.Add(Me.btnmoMaytinhMini)
+        Me.Controls.Add(Me.btnmoBanphimAo)
+        Me.Controls.Add(Me.btnmoProgramsAndFeatures)
+        Me.Controls.Add(Me.btntaiTeamViewer)
+        Me.Controls.Add(Me.btnmoAdapterOptions)
+        Me.Controls.Add(Me.btnmoAdvancedSharingSettings)
+        Me.Controls.Add(Me.btnmoDestopIconSettings)
+        Me.Controls.Add(Me.btntatwindowsupdate)
+        Me.Controls.Add(Me.btnmoNetplwiz)
+        Me.Controls.Add(Me.btnmoCmd)
+        Me.Controls.Add(Me.btnopenMsconfig)
+        Me.Controls.Add(Me.btnmoServices)
+        Me.Controls.Add(Me.btnmoGrouppolicy)
+        Me.Controls.Add(Me.btnmoregistry)
+        Me.Controls.Add(Me.btnmothumucHost)
+        Me.Controls.Add(Me.btnmothumucFonts)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Tiện ích hỗ trợ quản lý máy tính"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnmothumucFonts As Button
+    Friend WithEvents btnmothumucHost As Button
+    Friend WithEvents btnmoregistry As Button
+    Friend WithEvents btnmoGrouppolicy As Button
+    Friend WithEvents btnmoServices As Button
+    Friend WithEvents btnopenMsconfig As Button
+    Friend WithEvents btnmoCmd As Button
+    Friend WithEvents btnmoNetplwiz As Button
+    Friend WithEvents btntatwindowsupdate As Button
+    Friend WithEvents btnmoDestopIconSettings As Button
+    Friend WithEvents btnmoAdvancedSharingSettings As Button
+    Friend WithEvents btnmoAdapterOptions As Button
+    Friend WithEvents btntaiTeamViewer As Button
+    Friend WithEvents btnmoProgramsAndFeatures As Button
+    Friend WithEvents btnmoDateAndTime As Button
+    Friend WithEvents btnmoBanphimAo As Button
+    Friend WithEvents btnmoMaytinhMini As Button
+    Friend WithEvents btnmoPerformance As Button
+End Class
